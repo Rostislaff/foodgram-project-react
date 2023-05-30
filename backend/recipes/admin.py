3d83d4f0725ebfd5ca2f18840b131da3d4a9a294
Rostislaff <rossanna8368@gmail.com>
2023-05-30 22:52:25 +0300

    FG v1.2 api (2nd review)

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import FavoriteRecipe, Ingredient, Recipe, RecipeIngredient,\
-                    ShoppingCart, Subscribe, Tag
+from .models import (
+    FavoriteRecipe, Ingredient, Recipe,
+    RecipeIngredient, ShoppingCart, Subscribe, Tag
+)
 
 EMPTY_MSG = '-пусто-'
 
