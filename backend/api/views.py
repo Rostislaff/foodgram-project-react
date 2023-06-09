1eae9245568138c6ae81d74822abeb8f77ee9470
Rostislaff <rossanna8368@gmail.com>
2023-06-09 23:47:24 +0300

    foodgram review fix (admin,HEX_check,ttf)

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,7 +7,7 @@ from django.db.models.expressions import Exists, OuterRef, Value
 from django.http import FileResponse
 from django.shortcuts import get_object_or_404
 from .utils import ingredients_export
-from backend.recipes.models import RecipeIngredient
+from recipes.models import RecipeIngredient
 from djoser.views import UserViewSet
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
