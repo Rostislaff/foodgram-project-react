1b355d7bf9d680c5cd5154677d1fbf9044c5b71d
Rostislaff <rossanna8368@gmail.com>
2023-06-10 23:24:18 +0300

    foodgram review fix ingredients_search

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,4 @@
+import re
 import django.contrib.auth.password_validation as validators
 from django.contrib.auth import authenticate, get_user_model
 from django.contrib.auth.hashers import make_password
